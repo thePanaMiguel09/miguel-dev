491e6c5bd4768414117643a9cbcb360dd6b9d139
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-03-11 15:50:05 -0500

    tailwind configuration added with custom fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
@@ -7,4 +7,39 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "FiraCode-Bold",
+      cssVariable: "--font-firaCode",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/FiraCode-Bold.ttf"],
+            weight: "700",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/FiraCode-Regular.ttf"],
+            weight: "400",
+            style: "normal",
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "Inter",
+      cssVariable: "--font-inter",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/Inter-VariableFont_opsz,wght.ttf"],
+            style: "normal",
+            weight: "normal",
+          },
+        ],
+      },
+    },
+  ],
 });
