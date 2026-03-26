403eb7bb47da5b82bd853cc41237576e50ded57d
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-03-26 00:09:22 -0500

    SkillIoncsProps interface added for typing skillIcons

---
+++ src/data/skill_icons_data.ts
@@ -0,0 +1,93 @@
+interface SkillIconsProps {
+    skillName: string;
+    skillClassIcon: string;
+    colored: boolean;
+    shadowColor: string;
+}
+
+export const skillIcons: SkillIconsProps[] = [
+    {
+        skillName: 'HTML5',
+        skillClassIcon: 'devicon-html5-plain',
+        colored: true,
+        shadowColor: 'hover:drop-shadow-[0_0_8px_#e34f26]'
+    },
+    {
+        skillName: 'CSS3',
+        skillClassIcon: 'devicon-css3-plain',
+        colored: true,
+        shadowColor: 'hover:drop-shadow-[0_0_8px_#1572b6]'
+    },
+    {
+        skillName: 'Javascript',
+        skillClassIcon: 'devicon-javascript-plain',
+        colored: true,
+        shadowColor: 'hover:drop-shadow-[0_0_8px_#f7df1e]'
+    },
+    {
+        skillName: 'Typescript',
+        skillClassIcon: 'devicon-typescript-plain',
+        colored: true,
+        shadowColor: 'hover:drop-shadow-[0_0_8px_#3178c6]'
+    },
+    {
+        skillName: 'React',
+        skillClassIcon: 'devicon-react-original',
+        colored: true,
+        shadowColor: 'hover:drop-shadow-[0_0_8px_#61dafb]'
+    },
+    {
+        skillName: 'Tailwind CSS',
+        skillClassIcon: 'devicon-tailwindcss-original',
+        colored: true,
+        shadowColor: 'hover:drop-shadow-[0_0_8px_#06b6d4]'
+    },
+    {
+        skillName: 'Node.js',
+        skillClassIcon: 'devicon-nodejs-plain-wordmark',
+        colored: true,
+        shadowColor: 'hover:drop-shadow-[0_0_8px_#5fa04e]'
+    },
+    {
+        skillName: 'Express',
+        skillClassIcon: 'devicon-express-original',
+        colored: false,
+        shadowColor: 'hover:drop-shadow-[0_0_8px_#ffffff]'
+    },
+    {
+        skillName: 'PostgreSQL',
+        skillClassIcon: 'devicon-postgresql-plain',
+        colored: true,
+        shadowColor: 'hover:drop-shadow-[0_0_8px_#4169e1]'
+    },
+    {
+        skillName: 'MySQL',
+        skillClassIcon: 'devicon-mysql-plain',
+        colored: true,
+        shadowColor: 'hover:drop-shadow-[0_0_8px_#4479a1]'
+    },
+    {
+        skillName: 'Expo',
+        skillClassIcon: 'devicon-expo-line',
+        colored: false,
+        shadowColor: 'hover:drop-shadow-[0_0_8px_#ffffff]'
+    },
+    {
+        skillName: 'Dart',
+        skillClassIcon: 'devicon-dart-plain',
+        colored: true,
+        shadowColor: 'hover:drop-shadow-[0_0_8px_#0175c2]'
+    },
+    {
+        skillName: 'Flutter',
+        skillClassIcon: 'devicon-flutter-plain',
+        colored: true,
+        shadowColor: 'hover:drop-shadow-[0_0_8px_#54c5f8]'
+    },
+    {
+        skillName: 'Zustand',
+        skillClassIcon: 'devicon-zustand-plain',
+        colored: false,
+        shadowColor: 'hover:drop-shadow-[0_0_8px_#ffffff]'
+    },
+];
